c2a36dbb1948a3bde6759e1dcf05fc8ffbfd8fab
Rishabh5321 <40533251+Rishabh5321@users.noreply.github.com>
2025-03-03 09:43:00 +0530

    Refactor flake configuration and simplify package definition

--- flake.nix
+++ flake.nix
@@ -1,71 +1,67 @@
 {
-  description = "Seanime service flake";
+  description = "Seanime - Open-source media server for anime and manga";
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-    flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { self, nixpkgs, flake-utils }:
-    flake-utils.lib.eachDefaultSystem (system:
-      let
-        pkgs = nixpkgs.legacyPackages.${system};
-      in
-      {
-        packages = {
-          seanime = pkgs.stdenv.mkDerivation rec {
-            pname = "seanime";
-            version = "2.7.5";
+  outputs = { self, nixpkgs }: {
+    packages = let
+      system = "x86_64-linux";
+      pkgs = import nixpkgs { inherit system; };
+    in {
+      x86_64-linux.seanime = pkgs.stdenv.mkDerivation rec {
+        pname = "seanime";
+        version = "2.7.5";
 
-            src = pkgs.fetchurl {
-              url = "https://github.com/5rahim/seanime/releases/download/v${version}/seanime-${version}_Linux_x86_64.tar.gz";
-              sha256 = "sha256-Vtm+SkrUTUkVQQR4nrpc5pJXMORrBFYH4lj2R1BAFkY=";
-            };
-
-            phases = ["installPhase"];
-
-            installPhase = ''
-              mkdir -p $out/bin
-              tar xzf $src -C $out/bin
-            '';
-
-            meta = {
-              description = "Open-source media server with a web interface and desktop app for anime and manga";
-              homepage = "https://github.com/5rahim/seanime";
-              license = pkgs.lib.licenses.gpl3Only;
-            };
-          };
+        src = pkgs.fetchurl {
+          url = "https://github.com/5rahim/seanime/releases/download/v${version}/seanime-${version}_Linux_x86_64.tar.gz";
+          sha256 = "sha256-Vtm+SkrUTUkVQQR4nrpc5pJXMORrBFYH4lj2R1BAFkY=";
         };
 
-        nixosModules.seanime = { config, lib, pkgs, ... }: {
-          options.modules.home.services.seanime = {
-            enable = lib.mkEnableOption "seanime";
+        phases = [ "installPhase" ];
+
+        installPhase = ''
+          mkdir -p $out/bin
+          tar xzf $src -C $out/bin
+        '';
+
+        meta = {
+          description = "Open-source media server with a web interface and desktop app for anime and manga";
+          homepage = "https://github.com/5rahim/seanime";
+          license = pkgs.lib.licenses.gpl3Only;
+        };
+      };
+    };
+
+    defaultPackage.x86_64-linux = self.packages.x86_64-linux.seanime;
+
+    nixosModules.seanime = { pkgs, lib, config, ... }: {
+      options.modules.home.services.seanime = {
+        enable = lib.mkEnableOption "seanime";
+      };
+
+      config = lib.mkIf config.modules.home.services.seanime.enable {
+        home.packages = [
+          self.packages.x86_64-linux.seanime
+        ];
+
+        systemd.user.services.seanime = {
+          Unit = {
+            Description = "Seanime WebServer ";
+            After = "network.service";
+            X-SwitchMethod = "restart";
           };
 
-          config = lib.mkIf config.modules.home.services.seanime.enable {
-            home.packages = [
-              self.packages.${system}.seanime
-            ];
+          Install = {
+            WantedBy = [ "default.target" ];
+          };
 
-            systemd.user.services.seanime = {
-              Unit = {
-                Description = "Seanime WebServer ";
-                After = "network.service";
-                X-SwitchMethod = "restart";
-              };
-
-              Install = {
-                WantedBy = ["default.target"];
-              };
-
-              Service = {
-                ExecStart = "${self.packages.${system}.seanime}/bin/seanime";
-              };
-            };
+          Service = {
+            ExecStart = "${self.packages.x86_64-linux.seanime}/bin/seanime";
           };
         };
-
-        defaultPackage = self.packages.${system}.seanime;
-      }
-    );
+      };
+    };
+  };
 }
