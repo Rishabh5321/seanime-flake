acbc5c719365e4387cc770018f9b7ba3f1daae05
Rishabh5321 <40533251+Rishabh5321@users.noreply.github.com>
2025-04-13 10:29:28 +0530

    fix(seanime-pkg): change 'sha256' to 'hash' for consistency in fetchurl

--- seanime-pkg.nix
+++ seanime-pkg.nix
@@ -7,7 +7,7 @@ pkgs.stdenv.mkDerivation rec {
 
   src = pkgs.fetchurl {
     url = "https://github.com/5rahim/seanime/releases/download/v${version}/seanime-${version}_Linux_x86_64.tar.gz";
-    sha256 = "sha256-Vtm+SkrUTUkVQQR4nrpc5pJXMORrBFYH4lj2R1BAFkY=";
+    hash = "sha256-Vtm+SkrUTUkVQQR4nrpc5pJXMORrBFYH4lj2R1BAFkY=";
   };
 
   phases = [ "installPhase" ];
