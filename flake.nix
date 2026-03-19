5d6b50dfad2914922832ebf3324fe62b3b26f0fc
Rishabh5321 <40533251+Rishabh5321@users.noreply.github.com>
2026-03-19 12:04:50 +0000

    chore: auto lint and format

--- flake.nix
+++ flake.nix
@@ -5,28 +5,28 @@
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
   };
 
-  outputs = { self, nixpkgs }: 
-  let
-    archiveWarning = "WARNING: This repository (seanime-flake) is being ARCHIVED. Please move to https://github.com/Rishabh5321/custom-packages-flake for future updates.";
-  in
-  builtins.trace archiveWarning {
-    packages.x86_64-linux = {
-      seanime = import ./seanime-pkg.nix {
-        pkgs = import nixpkgs { system = "x86_64-linux"; };
+  outputs = { self, nixpkgs }:
+    let
+      archiveWarning = "WARNING: This repository (seanime-flake) is being ARCHIVED. Please move to https://github.com/Rishabh5321/custom-packages-flake for future updates.";
+    in
+    builtins.trace archiveWarning {
+      packages.x86_64-linux = {
+        seanime = import ./seanime-pkg.nix {
+          pkgs = import nixpkgs { system = "x86_64-linux"; };
+        };
+        default = self.packages.x86_64-linux.seanime;
       };
-      default = self.packages.x86_64-linux.seanime;
-    };
 
-    nixosModules.seanime = { pkgs, ... }: {
-      imports = [
-        ({ ... }: {
-          imports = [ ./seanime-home.nix ];
-          config = {
-            warnings = [ "seanime-flake: This repository is being ARCHIVED. Please move to https://github.com/Rishabh5321/custom-packages-flake for future updates." ];
-            _module.args.seanime = self.packages.x86_64-linux.seanime;
-          };
-        })
-      ];
+      nixosModules.seanime = { pkgs, ... }: {
+        imports = [
+          ({ ... }: {
+            imports = [ ./seanime-home.nix ];
+            config = {
+              warnings = [ "seanime-flake: This repository is being ARCHIVED. Please move to https://github.com/Rishabh5321/custom-packages-flake for future updates." ];
+              _module.args.seanime = self.packages.x86_64-linux.seanime;
+            };
+          })
+        ];
+      };
     };
-  };
 }
